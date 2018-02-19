6e6f01106f8c9ea2149c3a0ab7db08f51ed75cbd
Richard Gardner <richard.grdnr@gmail.com>
2018-02-19 11:50:48 +0100

    Rename project to "nrd2dat"

--- nrd2dat/stdafx.cpp
+++ nrd2dat/stdafx.cpp
@@ -1,5 +1,5 @@
 // stdafx.cpp : source file that includes just the standard includes
-// nrd2dat2.pch will be the pre-compiled header
+// nrd2dat.pch will be the pre-compiled header
 // stdafx.obj will contain the pre-compiled type information
 
 #include "stdafx.h"
